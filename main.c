4c25e2178125b2d59ff18fae61d180f5cf86065c
ian <ian.beaud@sfr.fr>
2026-08-30 13:56:18 +0200

    main.c updgrade

--- main.c
+++ main.c
@@ -21,8 +21,23 @@ void install(const char *paquet) {
         return;
     }
 
-    printf("Installation de %s terminée avec succès.\n", paquet);
 }
+void remove_package(const char *paquet) {
+    printf("Désinstallation de %s...\n", paquet);
+    char command[512];
+    // Cherche la recette nommé <paquet>-remove.sh
+    snprintf(command, sizeof(command),
+             "curl -fsSL https://raw.githubusercontent.com/Ianou68/Recipe-package-manager/main/remove/%s-remove.sh -o /tmp/%s-remove.sh && bash /tmp/%s-remove.sh && rm /tmp/%s-remove.sh",
+             paquet, paquet, paquet, paquet);
+
+    if (system(command) != 0) {
+        printf("Erreur ou recette de désinstallation introuvable pour %s.\n", paquet);
+        snprintf(command, sizeof(command), "rm -f /tmp/%s-remove.sh", paquet);
+        system(command);
+    }
+}
+
+
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -32,6 +47,8 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(argv[1], "install") == 0) {
         install(argv[2]);
+    } else if (strcmp(argv[1], "remove") == 0 || strcmp(argv[1], "uninstall") == 0) {
+        remove_package(argv[2]);
     } else {
         printf("Commande inconnue : %s\n", argv[1]);
         printf("Usage: %s install <paquet>\n", argv[0]);
